33342fa49fbe23aef41e8044d04ea8256da91828
Kun Zhou <zhoukun@u.nus.edu>
2021-11-17 23:14:55 +0800

    Update symbols.py

--- pre-train/reader/symbols.py
+++ pre-train/reader/symbols.py
@@ -1,3 +1,14 @@
+import os
+
+seen_speakers = []
+
+for seen_speaker in os.listdir('/data07/zhoukun/VCTK-Corpus/spec'):
+    seen_speakers.append(seen_speaker)
+
+seen_speakers.sort()
+seen_speakers = seen_speakers[:99]
+
+
 phone_list = ['pau', 'iy', 'aa', 'ch', 'ae', 'eh', 
  'ah', 'ao', 'ih', 'ey', 'aw', 
  'ay', 'ax', 'er', 'ng', 
@@ -9,21 +20,8 @@ phone_list = ['pau', 'iy', 'aa', 'ch', 'ae', 'eh',
  'uw', 'SOS/EOS']
 
 #seen_speakers = ['p336', 'p240', 'p262', 'p333', 'p297', 'p339', 'p276', 'p269', 'p303', 'p260', 'p250', 'p345', 'p305', 'p283', 'p277', 'p302', 'p280', 'p295', 'p245', 'p227', 'p257', 'p282', 'p259', 'p311', 'p301', 'p265', 'p270', 'p329', 'p362', 'p343', 'p246', 'p247', 'p351', 'p263', 'p363', 'p249', 'p231', 'p292', 'p304', 'p347', 'p314', 'p244', 'p261', 'p298', 'p272', 'p308', 'p299', 'p234', 'p268', 'p271', 'p316', 'p287', 'p318', 'p264', 'p313', 'p236', 'p238', 'p334', 'p312', 'p230', 'p253', 'p323', 'p361', 'p275', 'p252', 'p374', 'p286', 'p274', 'p254', 'p310', 'p306', 'p294', 'p326', 'p225', 'p255', 'p293', 'p278', 'p266', 'p229', 'p335', 'p281', 'p307', 'p256', 'p243', 'p364', 'p239', 'p232', 'p258', 'p267', 'p317', 'p284', 'p300', 'p288', 'p341', 'p340', 'p279', 'p330', 'p360', 'p285']
-#seen_speakers = ['awb','bdl','clb','slt','rms']
-
-seen_speakers = ['Neutral','Happy','Sad','Angry','Surprise']
 
 ph2id = {ph:i for i, ph in enumerate(phone_list)}
-ph2id['ssil'] = ph2id['pau']
-
-sp2id={'Neutral':0,'Happy':1,'Sad':2,'Angry':3,'Surprise':4}
-
-#sp2id={'awb':0,'bdl':1,'clb':2,'slt':3,'rms':4}
-#sp2id = {sp:i for i, sp in enumerate(seen_speakers)}
-
 id2ph = {i:ph for i, ph in enumerate(phone_list)}
-
-#id2sp = {i:sp for i, sp in enumerate(seen_speakers)}
-#id2sp = {0: 'awb', 1: 'bdl', 2: 'clb', 3: 'slt', 4: 'rms'}
-
-id2sp = {0: 'Neutral', 1: 'Happy', 2: 'Sad', 3: 'Angry', 4: 'Surprise'}
+sp2id = {sp:i for i, sp in enumerate(seen_speakers)}
+id2sp = {i:sp for i, sp in enumerate(seen_speakers)}
